b8a8bbcf335943fe16e9729c5b78818a4f74494a
KraftaN <63770425+KrftaN@users.noreply.github.com>
2023-09-15 13:09:01 +0200

    Add project files.

---
+++ Uppgift 2.5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data;
+
+namespace Uppgift_2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en uträkning");
+            string input = Console.ReadLine();
+            string summa = new DataTable().Compute(input, null).ToString();
+
+            Console.WriteLine("Svaret är: " + summa);
+            Console.ReadKey();
+        }
+    }
+}
